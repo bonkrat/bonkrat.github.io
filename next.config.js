71acaffe182eb928a9ff8998aafd7788a4267b84
bonkrat <1292831+bonkrat@users.noreply.github.com>
2022-12-16 15:21:27 -0500

    deploy action

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: isProd ? "/your-github-repo-name/" : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
