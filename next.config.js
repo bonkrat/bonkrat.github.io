522f60f73661beeff72db3181a62efd063a7307d
bonkrat <rossjernigan@gmail.com>
2022-12-16 16:27:17 -0500

    remove asset prefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? "/ross.fyi/" : "",
   images: {
     unoptimized: true,
   },
